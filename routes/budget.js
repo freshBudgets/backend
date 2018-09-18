be496a6637abf9c3a7ced35ee4024e349c35381d
Cole Johnson <colejohnson@Coles-MacBook-Pro.local>
2018-09-17 21:40:46 -0400

    Adds some more mock data

--- routes/budget.js
+++ routes/budget.js
@@ -18,6 +18,18 @@ const getAll = (req, res) => {
         total: 50.00,
         spent: 35.22
       },
+      3: {
+        id: 3,
+        name: 'Alcohol',
+        total: 300.00
+        spent: 354.00
+      },
+      4: {
+        id: 4,
+        name: 'Car Parts',
+        total: 100.00,
+        spent: 34.12
+      }
     }
   })
 }
