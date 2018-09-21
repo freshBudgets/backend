ae3e9d073c05bc744ccf34fa1423b72ab214a6f1
Keegan Irby <keegan.irby@gmail.com>
2018-09-21 16:37:06 -0400

    remove config

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  'mongoURI' : "mongodb+srv://freshbudgets:qxBaEwf5DZMLOx4J@cluster0-r65fp.mongodb.net/test?retryWrites=true",
-  'jwtSecret' : "superfreshsecret",
-  'jwtExpireTime' : 86400
-};
