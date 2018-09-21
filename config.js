50dfd99b9c989916d0e9198576306ab1d6f9b795
Keegan Irby <keegan.irby@gmail.com>
2018-09-21 16:33:52 -0400

    remove config from repo

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  'mongoURI' : "mongodb+srv://freshbudgets:qxBaEwf5DZMLOx4J@cluster0-r65fp.mongodb.net/test?retryWrites=true",
-  'jwtSecret' : "superfreshsecret",
-  'jwtExpireTime' : 86400,
-  'twilioSID': "AC11d1b9e939bd46da6571d7bbcf02806e",
-  'twilioToken': "14ab9695d7a9e37695d93c05a9a75f27",
-  'twilioPhoneNumber': "+12537331044"
-};
